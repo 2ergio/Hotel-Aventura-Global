e998558cb1ac9cca041d83fe59705f14f200dabf
2ergio <sergiohe903@gmail.com>
2024-11-25 22:03:05 -0500

    Commit html, css y js

---
+++ js/desplazamiento.js
@@ -0,0 +1,16 @@
+// JavaScript para ajustar el desplazamiento
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        const headerOffset = document.querySelector('header').offsetHeight;
+        const elementPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+        const offsetPosition = elementPosition - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
